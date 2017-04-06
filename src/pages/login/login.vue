db3772bd434162e597de7e84ab74cfabfffe18d2
jochen <18362961791@163.com>
2017-04-06 18:08:47 +0800

    add login

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -1,8 +1,35 @@
 <template>
+  <div class="loginContainer">
+    <div class="loginTitle">
 
+    </div>
+    <form class="loginForm">
+      <section class="input_container">
+        <input type="text" placeholder="用户名">
+      </section>
+      <section class="input_container">
+        <input type="password" placeholder="密码" v-model="passWord">
+        <div class="button_switch">
+          <div class="circle_button" v-on:click=""></div>
+          <span>abc</span>
+          <span>...</span>
+        </div>
+      </section>
+    </form>
+  </div>
 </template>
 
 <script>
   export default {
+      data(){
+          return {
+              showPassword: false //是否显示密码
+          }
+      },
+      methods: {
+          changePasswordType(){
+
+          }
+      }
   }
 </script>
